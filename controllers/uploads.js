4b7a382065cca490f2c6898886ba5943c6f26ab4
Mateo Lohezic <mateolohezic@gmail.com>
2023-06-09 15:05:22 -0300

    update etapa 2

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -2,6 +2,7 @@ const multer = require('multer');
 const { GridFsStorage } = require('multer-gridfs-storage');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const { Types: { ObjectId } } = require('mongoose');
 
 const conn = mongoose.createConnection(process.env.URL);
 
@@ -24,7 +25,7 @@ const storage = new GridFsStorage({
         filename: file.originalname,
         bucketName: 'uploads',
         metadata: {
-          userId: req.body // Add userId parameter from client side
+          userId: req.body, // Add userId parameter from client side
         }
       };
       resolve(fileInfo);
@@ -43,18 +44,21 @@ const uploadFile = (req, res) => {
     } else {
       const fileId = req.file.id;
       const userId = req.file.metadata.userId;
-      res.status(200).json({ success: true, fileId, userId });
+      const tipo = req.file.metadata.tipo;
+      res.status(200).json({ success: true, fileId, userId, tipo });
     }
   });
 };
 
-const getPdfs = async (req, res) => {
+const getPdfsEspecifico = async (req, res) => {
   try {
     const gfs = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
       bucketName: 'uploads'
     });
 
-    const files = await gfs.find({ 'metadata.userId.userId': req.params.userId }).sort({ uploadDate: -1 }).toArray();
+    const fileId = new ObjectId(req.params.fileId);
+
+    const files = await gfs.find({ '_id': fileId }).sort({ uploadDate: -1 }).toArray();
 
     if (files.length > 0) {
       const file = files[0];
@@ -71,5 +75,98 @@ const getPdfs = async (req, res) => {
   }
 };
 
+const getPdf = async (req, res) => {
+  try {
+    const gfs = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
+      bucketName: 'uploads'
+    });
 
-module.exports = { uploadFile, getPdfs };
+    const files = await gfs.find({ 'metadata.userId.userId': req.params.userId, 'metadata.userId.tipo': 'recibo'}).sort({ uploadDate: -1 }).toArray();
+
+    if (files.length > 0) {
+      const file = files[0];
+      const stream = gfs.openDownloadStream(file._id);
+      res.set('Content-Type', file.contentType);
+      res.set('Content-Disposition', `attachment; filename=${file.filename}`);
+      stream.pipe(res);
+    } else {
+      res.status(206).json({ message: 'No hay ningún comprobante cargado.' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(206).json({ error: 'Failed to get PDFs' });
+  }
+};
+
+const getPdfs = async (req, res) => {
+  try {
+    const gfs = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
+      bucketName: 'uploads'
+    });
+
+    const comprobantesFiles = await gfs.find({ 'metadata.userId.userId': req.params.userId, 'metadata.userId.tipo': 'comprobante' }).sort({ uploadDate: -1 }).toArray();
+    const recibosFiles = await gfs.find({ 'metadata.userId.userId': req.params.userId, 'metadata.userId.tipo': 'recibo' }).sort({ uploadDate: -1 }).toArray();
+
+    const comprobantesUrls = comprobantesFiles.map(file => {
+      const stream = gfs.openDownloadStream(file._id);
+      const uploadDate = new Date(file.uploadDate);
+      const formattedDate = uploadDate.toLocaleDateString('es-AR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+      }).split('/').join('/');
+      return {
+        id: file._id,
+        url: stream.url, // Replace with the appropriate method to generate a URL for the stream
+        name: file.filename,
+        date: formattedDate,
+        type: 'comprobante'
+      };
+    });
+
+    const recibosUrls = recibosFiles.map(file => {
+      const stream = gfs.openDownloadStream(file._id);
+      const uploadDate = new Date(file.uploadDate);
+      const formattedDate = uploadDate.toLocaleDateString('es-AR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+      }).split('/').join('/');
+      return {
+        id: file._id,
+        url: stream.url, // Replace with the appropriate method to generate a URL for the stream
+        name: file.filename,
+        date: formattedDate,
+        type: 'recibo'
+      };
+    });
+
+    const allUrls = [...comprobantesUrls, ...recibosUrls];
+
+    if (allUrls.length > 0) {
+      res.status(200).json({ comprobantes: comprobantesUrls, recibos: recibosUrls });
+    } else {
+      res.status(206).json({ message: 'No hay ningún comprobante o recibo cargado.' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(206).json({ error: 'Failed to get PDFs' });
+  }
+};
+
+const deleteFile = async (req, res) => {
+  try {
+    const fileId = new ObjectId(req.params.fileId);
+
+    const gfs = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
+      bucketName: 'uploads'
+    });
+
+    await gfs.delete(fileId);
+    res.status(200).json('Archivo eliminado con éxito.')
+  } catch (error) {
+    res.status(206).json(error.message)
+  }
+};
+
+module.exports = { uploadFile, getPdf, getPdfs, getPdfsEspecifico, deleteFile };
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { uploadFile, getPdfs, downloadFile } = require('../controllers/uploads');
+const { uploadFile, getPdf, getPdfs, getPdfsEspecifico, deleteFile } = require('../controllers/uploads');
 
 router.post('/upload-file', uploadFile);
+router.get('/getpdf-ultimo/:userId', getPdf);
 router.get('/getpdf/:userId', getPdfs);
+router.get('/getpdf-especifico/:fileId', getPdfsEspecifico);
+router.delete('/delete-pdf/:fileId', deleteFile);
 
 module.exports = router;
